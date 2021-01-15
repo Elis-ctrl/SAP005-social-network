10bffd39fb18cc5dd0a8e7d1569994cdfb3376ab
elis-ctrl <elis_emb@hotmail.com>
2021-01-15 20:20:57 -0300

    rotas corretas

--- src/pages/post/index.spec.js
+++ src/pages/post/index.spec.js
@@ -1,24 +1,10 @@
 import { Post } from './index.js';
-import * as services from '../../services/index.js';
 
 describe('Post', () => {
-  beforeEach(() => {
-    services.loginWithEmail = jest.fn(() => Promise.resolve(true));
-    services.loginGoogle = jest.fn(() => Promise.resolve(true));
-  });
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
   it('should be a function', () => {
     expect(typeof Post).toBe('function');
   });
   it('shoul return PostPage', () => {
     expect(Post()).toMatchSnapshot();
-  });
-  it('should click submmit button', () => {
-    const posTest = Post();
-    const btn = posTest.querySelector('#submit-home');
-    btn.dispatchEvent(new Event('click'));
-    expect(services.loginWithEmail).toHaveBeenCalledWith();
   });
 });
